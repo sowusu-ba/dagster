d41ccd85c3a784841259d2243de32e68cb327398
Sean Mackesey <s.mackesey@gmail.com>
2022-10-24 10:18:54 -0400

    restore pyproject.toml in docs_snippets

--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_io_manager.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_asset_io_manager.py
@@ -3,5 +3,5 @@ from docs_snippets.concepts.assets.asset_io_manager import my_repository
 
 def test():
     assert (
-        len(my_repository._assets_defs_by_key) == 2  # pylint: disable=protected-access
-    )
+        len(my_repository._assets_defs_by_key) == 2
+    )  # pylint: disable=protected-access
--- examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_io_managers_prod_local.py
+++ examples/docs_snippets/docs_snippets_tests/concepts_tests/assets_tests/test_io_managers_prod_local.py
@@ -7,18 +7,12 @@ from docs_snippets.concepts.assets.asset_io_manager_prod_local import my_reposit
 @mock.patch.dict(os.environ, {"ENV": "prod"})
 def test_prod_assets():
     assert (
-        len(
-            my_repository._assets_defs_by_key.keys()  # pylint: disable=protected-access
-        )
-        == 2
-    )
+        len(my_repository._assets_defs_by_key.keys()) == 2
+    )  # pylint: disable=protected-access
 
 
 @mock.patch.dict(os.environ, {"ENV": "local"})
 def test_local_assets():
     assert (
-        len(
-            my_repository._assets_defs_by_key.keys()  # pylint: disable=protected-access
-        )
-        == 2
-    )
+        len(my_repository._assets_defs_by_key.keys()) == 2
+    )  # pylint: disable=protected-access
